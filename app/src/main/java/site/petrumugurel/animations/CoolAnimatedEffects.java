98db1914f68eb8fb24dfad3bfe0380624f6a8288
Mugurell <petrumugurel@gmail.com>
2016-02-12 18:15:45 +0200

    Finished my study on simple Animations.
    App still have >=2 bugs :
         - sometimes the animations overlap: asking bart to rotate on X has effect on X rotation of Homer.
         - sometimes the bart image cannot be rotated on X. Although they both use same implementation.

--- app/src/main/java/site/petrumugurel/animations/CoolAnimatedEffects.java
+++ app/src/main/java/site/petrumugurel/animations/CoolAnimatedEffects.java
@@ -3,6 +3,8 @@ package site.petrumugurel.animations;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -10,43 +12,113 @@ import java.util.concurrent.TimeUnit;
  * Kanged from https://github.com/GeMoschen/Stundenrechner/blob/master/src/de/gemo/stunden/units/OnSwipeTouchListener.java, By GeMoschen
  */
 public class CoolAnimatedEffects  {
-    private final View    mWorkedOnView;
+    public enum DIRECTIONS { RIGHT, LEFT }
+
+    private final ImageView mWorkedOnView;
+    private int mTranslationX = 2000;
 
     private long mDownSwipeStartTime = System.nanoTime();
-    private long mUpSwipeStartTime = System.nanoTime();
+    private long mUpSwipeStartTime   = System.nanoTime();
 
     private int mSwypeDownCounter;
     private int mSwypeUpCounter;
 
     public CoolAnimatedEffects(View view) {
-        mWorkedOnView = view;
+        mWorkedOnView = (ImageView) view;
     }
 
-    public void rotateOnXInPlace(float degrees, int duration) {
+    public void rotateOnXInPlace(int swipesNumber, int swipesBlockSeconds,
+                                 float degrees, int duration) {
         if (TimeUnit.SECONDS.convert(System.nanoTime() - mDownSwipeStartTime,
-                                     TimeUnit.NANOSECONDS) > 5) {
+                                     TimeUnit.NANOSECONDS) > swipesBlockSeconds) {
             mDownSwipeStartTime = System.nanoTime();
-            mSwypeDownCounter = 0;
+            mSwypeUpCounter = mSwypeDownCounter = 0;
         }
-        else {
-            if (++mSwypeDownCounter == 4) {
-                mSwypeDownCounter = 0;
-                mWorkedOnView.animate().rotationXBy(degrees).setDuration(duration);
-            }
+        else if (++mSwypeDownCounter == swipesNumber) {
+            mSwypeUpCounter = mSwypeDownCounter = 0;
+            mWorkedOnView.animate().rotationXBy(degrees).setDuration(duration);
+        }
+        else if (++mSwypeUpCounter == swipesNumber) {
+            mSwypeUpCounter = mSwypeDownCounter = 0;
+            mWorkedOnView.animate().rotationXBy(degrees).setDuration(duration);
         }
     }
 
-    public void fadeInRotatingImage(ImageView nextImage, ImageView previousImage,
+    public void fadeInRotatingImage(ImageView nextImage, final DIRECTIONS fadeInFrom,
                                     float rotationDegrees, int durationIn, int durationOut) {
-        nextImage.setTranslationX(-2000);
+
+        if (fadeInFrom == DIRECTIONS.RIGHT) {
+            mTranslationX = -mTranslationX;
+        }
+
+        nextImage.setVisibility(View.VISIBLE);
+        nextImage.setTranslationX(mTranslationX);
         nextImage.animate().alpha(0);
         nextImage.setRotation(0);
         nextImage.setScaleX(0);
         nextImage.setScaleY(0);
 
-        nextImage.animate().alpha(1).translationXBy(2000).rotation(rotationDegrees)
-                 .scaleX(1).scaleY(1).setDuration(durationIn);
+        nextImage.animate().alpha(1).translationXBy(-mTranslationX).rotation(rotationDegrees)
+                     .scaleX(1).scaleY(1).setDuration(durationIn);
 
-        previousImage.animate().alpha(0).setDuration(durationOut);
+        mWorkedOnView.animate().alpha(0).setDuration(durationOut);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (fadeInFrom == DIRECTIONS.RIGHT) {
+                    mWorkedOnView.animate().translationX(mTranslationX).scaleY(0).scaleX(0);
+                }
+                else {
+                    mWorkedOnView.animate().translationX(-mTranslationX).scaleY(0).scaleX(0);
+                }
+            }
+        }, durationOut/2);
+
+
+        // restore the original value of mTranslationX
+        if (fadeInFrom == DIRECTIONS.RIGHT) {
+            mTranslationX = -mTranslationX;
+        }
+    }
+
+
+    public void fadeInImage(ImageView nextImage, final DIRECTIONS fadeInFrom,
+                            int durationIn, final int durationOut) {
+
+        if (fadeInFrom == DIRECTIONS.RIGHT) {
+            mTranslationX = -mTranslationX;
+        }
+
+
+        nextImage.setVisibility(View.VISIBLE);
+        nextImage.setTranslationX(mTranslationX);
+        nextImage.setRotationX(0);
+        nextImage.setScaleX(0);
+        nextImage.setScaleY(0);
+        nextImage.animate().alpha(0).setDuration(1);
+
+
+        nextImage.animate().alpha(1).translationXBy(-mTranslationX)
+                     .scaleX(1).scaleY(1).setDuration(durationIn);
+
+        mWorkedOnView.animate().alpha(0).setDuration(durationOut);
+
+        // restore the original value of mTranslationX
+        if (fadeInFrom == DIRECTIONS.RIGHT) {
+            mTranslationX = -mTranslationX;
+        }
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (fadeInFrom == DIRECTIONS.RIGHT) {
+                    mWorkedOnView.animate().translationX(mTranslationX).scaleY(0).scaleX(0);
+                }
+                else {
+                    mWorkedOnView.animate().translationX(-mTranslationX).scaleY(0).scaleX(0);
+                }
+            }
+        }, durationOut/2);
     }
 }
--- app/src/main/java/site/petrumugurel/animations/MainActivity.java
+++ app/src/main/java/site/petrumugurel/animations/MainActivity.java
@@ -4,7 +4,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,18 +11,13 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.util.concurrent.TimeUnit;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private ImageView mBartImageView;
-    private ImageView mHomerImageView;
-    private Button    mBartBTN;
-    private Button    mHomerBTN;
-
-    int  mSwypeDownCounter   = 0;
-    long mDownSwipeStartTime = System.nanoTime();
-    int  mSwypeUpCounter     = 0;
-    long mUpSwipeStartTime   = System.nanoTime();
+    private ImageView           mBartImageView;
+    private ImageView           mHomerImageView;
+    private Button              mBartBTN;
+    private Button              mHomerBTN;
+    private CoolAnimatedEffects mHomerAnimator;
+    private CoolAnimatedEffects mBartAnimator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,37 +26,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
         mBartImageView = (ImageView) findViewById(R.id.mainA_RL_IV_bart);
+//        mBartImageView.setVisibility(View.INVISIBLE);
+        mBartImageView.animate().alpha(0);
+        mBartAnimator = new CoolAnimatedEffects(mBartImageView);
         mBartImageView.setOnTouchListener(new OnSwipeTouchListener(this) {
             @Override
             public boolean onSwipeDown() {
-//                Toast.makeText(MainActivity.this, "Ai dat jos", Toast.LENGTH_SHORT).show();
-                if (TimeUnit.SECONDS.convert(System.nanoTime() - mDownSwipeStartTime,
-                                             TimeUnit.NANOSECONDS) > 5) {
-                    mDownSwipeStartTime = System.nanoTime();
-                    mSwypeDownCounter = 0;
-                }
-                else {
-                    if (++mSwypeDownCounter == 4) {
-                        mSwypeDownCounter = 0;
-                        rotateInPlace(mBartImageView);
-                    }
-                }
-
+                mBartAnimator.rotateOnXInPlace(2, 5, -2160, 2000);
                 return  true;
             }
 
             @Override
             public boolean onSwipeUp() {
-                Toast.makeText(MainActivity.this, "Ai dat sus", Toast.LENGTH_SHORT).show();
+                mBartAnimator.rotateOnXInPlace(2, 5, 2160, 2000);
+                return true;
+            }
+
+            @Override
+            public boolean onSwipeRight() {
+                mBartAnimator.fadeInRotatingImage(mHomerImageView,
+                                                  CoolAnimatedEffects.DIRECTIONS.RIGHT,
+                                                  360, 2000, 2000);
+                return true;
+            }
+
+            @Override
+            public boolean onSwipeLeft() {
+                showToastOnePicture();
                 return true;
             }
         });
+
         mHomerImageView = (ImageView) findViewById(R.id.mainA_RL_IV_homer);
+        mHomerAnimator = new CoolAnimatedEffects(mHomerImageView);
+        mHomerImageView.setOnTouchListener(new OnSwipeTouchListener(this) {
+            @Override
+            public boolean onSwipeDown() {
+                mHomerAnimator.rotateOnXInPlace(2, 5, -2160, 2000);
+                return true;
+            }
 
+            @Override
+            public boolean onSwipeUp() {
+                mHomerAnimator.rotateOnXInPlace(2, 5, 2160, 2000);
+                return true;
+            }
 
-//        mBartImageView.setVisibility(View.GONE);
-        mBartImageView.animate().alpha(0);  // his dad we'll be on top
-//        mBartImageView.animate().translationX(2000);
+            @Override
+            public boolean onSwipeLeft() {
+                mHomerAnimator.fadeInRotatingImage(mBartImageView,
+                                                   CoolAnimatedEffects.DIRECTIONS.LEFT,
+                                                   360, 2000, 2000);
+                return true;
+            }
+
+            @Override
+            public boolean onSwipeRight() {
+                showToastOnePicture();
+                return true;
+            }
+        });
 
         mHomerBTN = (Button) findViewById(R.id.mainA_RL_LL_btn_homer);
         mBartBTN = (Button) findViewById(R.id.mainA_RL_LL_btn_bart);
@@ -71,45 +94,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mBartBTN.setOnClickListener(this);
     }
 
-
-    /**
-     * Receives another View to fade in to display in our ImageView.
-     * @param newImageToDisplay will fade in, and previous image will fade out.
-     */
-    private void fadeInImage(View newImageToDisplay) {
-        if ((newImageToDisplay).getId() == mHomerImageView.getId()) {
-            mHomerImageView.setTranslationX(-2000);
-            mHomerImageView.animate().alpha(0);
-            mHomerImageView.setRotation(0);
-            mHomerImageView.setScaleX(0);
-            mHomerImageView.setScaleY(0);
-
-            mHomerImageView.animate().alpha(1).translationXBy(2000)
-                           .rotation(360).scaleX(1).scaleY(1)
-                           .setDuration(2500);
-
-            mBartImageView.animate().alpha(0).setDuration(2000);
-        }
-        else if (( newImageToDisplay).getId() == mBartImageView.getId()) {
-            mBartImageView.setTranslationX(2000f);
-            mBartImageView.animate().alpha(0);
-            mBartImageView.setRotation(0);
-            mBartImageView.setScaleX(0);
-            mBartImageView.setScaleY(0);
-
-            mBartImageView.animate().alpha(1).translationXBy(-2000)
-                          .rotation(360).scaleX(1).scaleY(1)
-                          .setDuration(2500);
-
-            mHomerImageView.animate().alpha(0).setDuration(2000);
-        }
-    }
-
     private void showToastOnePicture() {
-/*        Toast.makeText(MainActivity.this,
-                       "Only one picture available.\nYou don't like it?",
-                       Toast.LENGTH_SHORT).show();*/
-
         Snackbar snackbar = Snackbar
                 .make(findViewById(android.R.id.content),
                       "Only one picture available.\nYou don't like it?",
@@ -152,12 +137,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         int buttonId = v.getId();
-//        Log.e("pressedButton: ", Integer.toString(buttonId));
-//        Log.e("bartBtnID: ", Integer.toString(mBartBTN.getId()));
-//        Log.e("homerBrnID: ", Integer.toString(mHomerBTN.getId()));
         if (buttonId == mBartBTN.getId()) {
             if (mBartImageView.getAlpha() < 1) {
-                fadeInImage(mBartImageView);
+                mHomerAnimator.fadeInImage(mBartImageView,
+                                           CoolAnimatedEffects.DIRECTIONS.LEFT, 2000, 2000);
             }
             else {
                 showToastOnePicture();
@@ -165,17 +148,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         else if (buttonId == mHomerBTN.getId()) {
             if (mHomerImageView.getAlpha() < 1) {
-                fadeInImage(mHomerImageView);
+                mBartAnimator.fadeInImage(mHomerImageView,
+                                          CoolAnimatedEffects.DIRECTIONS.RIGHT, 2000, 2000);
             }
             else {
                 showToastOnePicture();
             }
         }
-    }
-
-    private void rotateInPlace(View v) {
-        Log.e("rotateInPlace ", "called");
-        v.setRotationY(0);
-        v.animate().rotationXBy(-3600).setDuration(2000);
     }
 }
